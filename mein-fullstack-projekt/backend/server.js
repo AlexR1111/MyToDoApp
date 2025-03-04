797baebffd50ad61c32a8a1b7971607630d7fc5e
AlexR1111 <148652561+AlexR1111@users.noreply.github.com>
2025-03-04 14:29:55 +0100

    Update server.js

--- mein-fullstack-projekt/backend/server.js
+++ mein-fullstack-projekt/backend/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const sqlite3 = require('sqlite3').verbose(); // verbose hilft beim fehler finden
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 5000;
+
+const db = new sqlite3.Database('./tasks.db',(err) => {
+  if (err) {
+    console.error('Datenbank konnte nicht geöffnet werden', err.message);
+  } else {
+    console.log('Mit Datenbank verbunden');
+  }
+});
+
+app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
+
+
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
 });
